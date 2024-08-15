19791caafba23adb06be51901bb5139a685b321c
ibrahim <ibrahimsaad6677@gmail.com>
2024-08-15 12:01:46 +0300

    complates

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/env python3
+
+'''
+    Basic annotation for variables.
+'''
+
+def add(a: float, b: float) -> float:
+    '''
+        Return sum of two floats.
+    '''
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""Basic annotations concat """
+
+def concat(srt1: str, str2: str) -> str:
+    """Return sum of two string"""
+    return srt1 + str2
