6cde1df4c5fddb210458cc80ff86a99a94ffe87f
ibrahim <ibrahimsaad6677@gmail.com>
2024-08-15 12:44:12 +0300

    complate 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """ Complex types - string and int/float to tuple"""
 from typing import Callable, Iterator, Union, Optional, List, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     takes a string k and an int OR float v as arguments
     returns a tuple.
     """
-    
     return (k, v**2)
